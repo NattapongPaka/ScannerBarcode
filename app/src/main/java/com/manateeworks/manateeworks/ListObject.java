9fb5bce6eed71520ea492d33872def9ae0e55a8b
Nattapong Paka <nattapongpaka@Nattapongs-MacBook-Pro.local>
2017-03-21 13:28:07 +0700

    push first

---
+++ app/src/main/java/com/manateeworks/manateeworks/ListObject.java
@@ -0,0 +1,13 @@
+package com.manateeworks.manateeworks;
+
+/**
+ * Created by lazarilievski on 1/25/17.
+ */
+
+public class ListObject {
+
+    public String name = "";
+    public String type = "";
+    public String code = "";
+
+}
